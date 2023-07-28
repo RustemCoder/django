cfd08d6a0b9e406df9b5f998007f7e7686c9178d
Rustem Aisariyev <scorus0808@gmail.com>
2023-07-27 20:04:32 -0400

    added local changes to remote repo

---
+++ littlelemon/littlelemonapi/views.py
@@ -0,0 +1,52 @@
+from django.db import IntegrityError
+from django.http import HttpResponse, JsonResponse
+from .models import Book
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
+from rest_framework.renderers import TemplateHTMLRenderer,StaticHTMLRenderer
+from rest_framework.decorators import api_view, renderer_classes
+# Create your views here.
+@csrf_exempt
+def books(request):
+    if request.method == 'GET':
+        books = Book.objects.all().values()
+        return JsonResponse({"books":list(books)})
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        price = request.POST.get('price')
+        book = Book(
+            title = title,
+            author = author,
+            price = price
+        )
+        try:
+            book.save()
+        except IntegrityError:
+            return JsonResponse({'error':'true','message':'required field missing'},status=400)
+
+        return JsonResponse(model_to_dict(book), status=201)
+
+@api_view
+def book(request):
+    return Response('list of books',status = status.HTTP_200_OK)
+
+class BookView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class SingleBookView(generics.RetrieveUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+@api_view(['GET'])
+@renderer_classes([StaticHTMLRenderer])
+def welcome(request):
+    data = '<html><body><h1>Welcome To Little Lemon API Project</h1></body></html>'
+    return Response(data)
